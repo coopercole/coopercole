f232eef84949ad363d88c612f22f747439ea7b28
Cooper Cole <c4cole@uwaterloo.ca>
2021-11-12 00:02:27 -0500

    slight updates

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-// var bgcolorlist = ['linear-gradient(#83f5e5, #e761bd)', 'linear-gradient(#a8f368, #f9035e)', 
-// 'linear-gradient(#60efff, #0061ff)', 'linear-gradient(#f3f98a, #95ecb0)', 'linear-gradient(#45cde9, #7a8fd3)', 
-// 'linear-gradient(#c3e1fc, #dd83ad)'  ];
+// nothing to see here...
+
+// (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+//   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+//   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+//   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  
+//   ga('create', 'UA-XXXXX-Y', 'auto');
+//   ga('send', 'pageview');
 
 
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
 
 
 
